5a64e6c0b4dd31408bbf654d66bc6c0b06d4a8af
saydul-kader <saydul.kader@gmail.com>
2021-05-07 01:59:33 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mini ecommerce app backend
+ This the backend of the mini ecommerce app with MERN stack.
---
+++ data/products.js
@@ -0,0 +1,77 @@
+const products = [
+  {
+    name: "Iphone 12",
+    imageUrl:
+      "https://images.unsplash.com/photo-1603625953304-97b6e41336b5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    description:
+      "Welcome to a new era of iPhone. Beautifully bright 6.1-inch Super Retina XDR display.1 Ceramic Shield with 4x better drop performance.2 Incredible low-light photography with Night mode on all cameras. Cinema-grade Dolby Vision video recording, editing, and playback. Powerful A14 Bionic chip. And new MagSafe accessories for easy attach and faster wireless charging.3 Let the fun begin.",
+    price: 1099,
+    countInStock: 10,
+  },
+  {
+    name: "PlayStation 5",
+    imageUrl:
+      "https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+    description:
+      "PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. Announced in 2019 as the successor to the PlayStation 4, the PS5 was released on November 12, 2020 in Australia, Japan, New Zealand, North America, Singapore, and South Korea, and November 19, 2020 onwards in other major markets except China and India.",
+    price: 499,
+    countInStock: 15,
+  },
+  {
+    name: "Cannon EOS-1D",
+    imageUrl:
+      "https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    description:
+      "The EOS-1D X combines speed with image quality, to create the next generation camera for professionals. Full frame 18 megapixel sensor with Dual “DIGIC 5+” processors sets the standard, and up to 12 frames per second shooting takes it beyond.",
+    price: 1300,
+    countInStock: 5,
+  },
+  {
+    name: "Google Home mini",
+    imageUrl:
+      "https://images.unsplash.com/photo-1546800409-51b20e2a4433?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    description:
+      "Google Home, is a line of smart speakers developed by Google under the Google Nest brand. The devices enable users to speak voice commands to interact with services through Google Assistant, the company's virtual assistant.",
+    price: 100,
+    countInStock: 10,
+  },
+  {
+    name: "Amazon Alexa",
+    imageUrl:
+      "https://images.unsplash.com/photo-1583505093722-15596e9ada6f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    description:
+      "It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system.",
+    price: 50,
+    countInStock: 25,
+  },
+  {
+    name: "Audio Technica Headphones",
+    imageUrl:
+      "https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    description:
+      "Outfitted with 45mm large-aperture dynamic drivers and an over-ear, closed-back design, the ATH-M50x headphones deliver clarity, deep bass, and extended bandwidth (15 Hz to 28 kHz) while isolating you from outside sounds.",
+    price: 233,
+    countInStock: 4,
+  },
+  {
+    name: "JBL FLIP 4",
+    imageUrl:
+      "https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",
+    description:
+      "JBL Flip 4 is the next generation in the award-winning Flip series; it is a portable Bluetooth speaker that delivers surprisingly powerful stereo sound. This compact speaker is powered by a 3000mAh rechargeable Li-ion battery that offers up to 12 hours of continuous, high-quality audio playtime.",
+    price: 140,
+    countInStock: 10,
+  },
+  {
+    name: "Apple Smart Watch",
+    imageUrl:
+      "https://images.unsplash.com/photo-1579586337278-3befd40fd17a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80",
+    description:
+      "Apple Watch is a wearable smartwatch that allows users to accomplish a variety of tasks, including making phone calls, sending text messages and reading email.",
+    price: 499,
+    countInStock: 20,
+  }
+  
+];
+
+module.exports = products;
